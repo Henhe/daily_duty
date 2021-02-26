2749143f4d51a98747504eff2f184d33918fe5e7
Goncharov Oleg <goncharovoleg1979@gmail.com>
2021-02-26 09:22:27 +0300

    add tables

--- daily_duty/duty/admin.py
+++ daily_duty/duty/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Month
+from .models import Period
+from .models import WorkHours
+from .models import Employees
+
 admin.site.register(Month)
+admin.site.register(Period)
+
+class WhAdmin(admin.ModelAdmin) :
+	list_display = ('period', 'days_quantity', 'workhours_sum')
+	search_fields = ('period',)
+
+admin.site.register(WorkHours, WhAdmin)
+
+class EmployeesAdmin(admin.ModelAdmin) :
+	list_display = ('full_name', 'surname', 'name', 'middle_name')
+	search_fields = ('surname',)
+
+admin.site.register(Employees, EmployeesAdmin)
--- daily_duty/duty/models.py
+++ daily_duty/duty/models.py
@@ -16,3 +16,150 @@ class Month(models.Model):
         unique_together = ('name', 'name_int')
     def __str__(self):
         return self.name
+
+class Period(models.Model):
+
+    year = models.PositiveSmallIntegerField(verbose_name='Период год',
+                                                validators=[validators.MinValueValidator(2017),
+                                                            validators.MaxValueValidator(2040)])
+    month = models.ForeignKey('Month', verbose_name='Период месяц', on_delete=models.PROTECT)
+    class Meta:
+        verbose_name_plural = 'Периоды'
+        verbose_name = 'Период'
+        ordering = ['-year', 'month']
+        unique_together = ('year', 'month')
+    def __str__(self):
+        return f'{self.month} {self.year}'
+
+class WorkHours(models.Model):
+    period = models.ForeignKey('Period', verbose_name='Период', on_delete=models.PROTECT)
+    days_quantity = models.PositiveSmallIntegerField(default=28, verbose_name='Количество дней', help_text='количество дней в месяце',
+                                                    validators=[validators.MinValueValidator(28),
+                                                                validators.MaxValueValidator(31)])
+    day1= models.DecimalField(max_digits=4, decimal_places=2, verbose_name='1', help_text='рабочее время, ч.',
+                              default=0, validators=[validators.MinValueValidator(0),
+                                                            validators.MaxValueValidator(24)])
+    day2 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='2', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day3 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='3', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day4 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='4', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day5 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='5', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day6 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='6', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day7 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='7', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day8 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='8', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day9 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='9', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day10 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='10', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day11 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='11', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day12 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='12', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day13 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='13', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day14 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='14', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day15 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='15', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day16 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='16', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day17 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='17', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day18 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='18', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day19 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='19', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day20 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='20', help_text='рабочее время, ч.',
+                               default=0, validators=[validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(24)])
+    day21 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='21', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day22 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='22', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day23 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='23', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day24 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='24', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day25 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='25', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day26 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='26', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day27 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='27', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day28 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='28', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day29 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='29', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day30 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='30', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+    day31 = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='31', help_text='рабочее время, ч.',
+                                default=0, validators=[validators.MinValueValidator(0),
+                                            validators.MaxValueValidator(24)])
+
+    def workhours_sum(self):
+        return self.day1 + self.day2 + self.day3 + self.day4 + self.day5 + self.day6 + self.day7 + self.day8 + self.day9 + self.day10 + \
+        self.day11 + self.day12 + self.day13 + self.day14 + self.day15 + self.day16 + self.day17 + self.day18 + self.day19 + self.day20 + \
+        self.day21 + self.day22 + self.day23 + self.day24 + self.day25 + self.day26 + self.day27 + self.day28 + self.day29 + self.day30 + \
+        self.day31
+
+    workhours_sum.short_description ='Рабочее время сумма'
+
+    class Meta:
+        verbose_name_plural = 'Рабочее время месяцы'
+        verbose_name = 'Рабочее время месяц'
+        # ordering = ['-year', 'month']
+        # unique_together = ('year', 'month')
+
+class Employees(models.Model):
+    surname = models.CharField(max_length=40, verbose_name='Фамилия',
+                            validators=[validators.RegexValidator("^['А-Я']['а-я']+",
+                                                                  message='фамилия должна начинаться с большой буквы, кириллицей')])
+    name = models.CharField(max_length=20, verbose_name='Имя',
+                            validators=[validators.RegexValidator("^['А-Я']['а-я']+",
+                            message='имя должно начинаться с большой буквы, кириллицей')])
+    middle_name = models.CharField(max_length=20, verbose_name='Отчество',
+                            validators=[validators.RegexValidator("^['А-Я']['а-я']+",
+                                                                  message='отчество должно начинаться с большой буквы, кириллицей')])
+
+    def full_name(self):
+        return f'{self.surname} {self.name[0:1]}. {self.middle_name[0:1]}.'
+
+    full_name.short_description = 'ФИО'
+
+    class Meta:
+        verbose_name_plural = 'Сотрудники'
+        verbose_name = 'Сотрудник'
